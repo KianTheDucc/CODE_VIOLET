ba3b6cef4c3165934170043f7e42635311aa8a61
OSCAR SMITH <ES294335@falmouth.ac.uk>
2024-03-06 12:21:26 +0000

    Reworked scene, added script to move down platforms

---
+++ Assets/Scripts/moveThroughGround.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class moveThroughGround : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") == -1f)
+        {
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            Debug.Log("PlayerFallThrough");
+        }
+        else
+        {
+            if (Input.GetAxisRaw("Vertical") == 1f)
+            {
+                this.GetComponent<BoxCollider2D>().enabled = true;
+            }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    this.GetComponent<BoxCollider2D>().enabled = true;
+                }
+            }
+        }
+    }
+}
