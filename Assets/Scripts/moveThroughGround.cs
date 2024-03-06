ce70437d8bb20b02f07ae748670918c6b85fa978
KianTheDucc <kiantburns1007@gmail.com>
2024-03-06 18:05:05 +0000

    Fixed player falling through a platform falling through all platforms with the script

--- Assets/Scripts/moveThroughGround.cs
+++ Assets/Scripts/moveThroughGround.cs
@@ -6,21 +6,24 @@ using UnityEngine;
 public class moveThroughGround : MonoBehaviour
 {
     public float passThroughTime;
+    private GameObject platform;
+    public MovementData Data;
 
     void Update()
     {
         if (Input.GetAxisRaw("Vertical") == -1f)
         {
-            StartCoroutine(disableCollider());
+            StartCoroutine(platformFallThrough());
         }
     }
 
-    private IEnumerator disableCollider()
+    public IEnumerator platformFallThrough()
     {
-        GetComponent<BoxCollider2D>().enabled = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Data.groundcastDistance, Data.whatIsGround);
+        hit.collider.enabled = false;
         Debug.Log("PlayerFallThrough");
         yield return new WaitForSeconds(passThroughTime);
-        GetComponent<BoxCollider2D>().enabled = true;
+        hit.collider.enabled = true;
         Debug.Log("Collider reenabled");
     }
 }
