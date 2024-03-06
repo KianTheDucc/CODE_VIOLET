4d9ca6740cc51aa34a90a82ba86843ca365d4d52
KianTheDucc <kiantburns1007@gmail.com>
2024-03-06 17:47:12 +0000

    Changed PlayerFallThrough to enable the collider after the player has fallen through

--- Assets/Scripts/moveThroughGround.cs
+++ Assets/Scripts/moveThroughGround.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 
 public class moveThroughGround : MonoBehaviour
 {
+    public float passThroughTime;
+
     void Update()
     {
         if (Input.GetAxisRaw("Vertical") == -1f)
         {
-            this.GetComponent<BoxCollider2D>().enabled = false;
-            Debug.Log("PlayerFallThrough");
+            StartCoroutine(disableCollider());
         }
-        else
-        {
-            if (Input.GetAxisRaw("Vertical") == 1f)
-            {
-                this.GetComponent<BoxCollider2D>().enabled = true;
-            }
-        }
+    }
+
+    private IEnumerator disableCollider()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        Debug.Log("PlayerFallThrough");
+        yield return new WaitForSeconds(passThroughTime);
+        GetComponent<BoxCollider2D>().enabled = true;
+        Debug.Log("Collider reenabled");
     }
 }
