9636ff007d5d4e5fc3e93df92b309b3b5734c586
KianTheDucc <kiantburns1007@gmail.com>
2024-03-09 15:42:18 +0000

    Added raycast functionality for RangedAlert

--- Assets/Scripts/Enemy/RangedAlert.cs
+++ Assets/Scripts/Enemy/RangedAlert.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class RangedAlert : MonoBehaviour
+public class RangedHit : MonoBehaviour
 {
+    public float LOSDistance;
+    public LayerMask player;
+    private float angleIncrement;
+    public float LOSAngle;
+    private bool isAlert;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (RangedLOS())
+        {
+            Alerted();
+        }
     }
+
+    public void Alerted()
+    {
+        Debug.Log("alerted");
+    }
+
+    #region CONDITIONS
+
+    public bool RangedLOS()
+    {
+        isAlert = false;
+        angleIncrement = 1f;
+        if(transform.rotation.eulerAngles.y == 180f)
+        {
+            LOSAngle = 360f;
+        }
+            for (float angle = 0f; angle < LOSAngle; angle += angleIncrement)
+            {
+                if (transform.rotation.eulerAngles.y == 180f)
+                {
+                    angle += 270;
+                }
+                float angleRadians = angle * Mathf.Deg2Rad;
+                Vector2 direction = new Vector2(Mathf.Cos(angleRadians), Mathf.Sin(angleRadians));
+                var hit = Physics2D.Raycast(transform.position, direction, LOSDistance, player);
+                Debug.DrawLine(transform.position, hit.point);
+                if (hit.collider != null)
+                {
+                    isAlert = true;
+                }
+            }
+
+
+
+        return isAlert;
+    }
+
+    #endregion
 }
