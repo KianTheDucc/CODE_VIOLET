aa0bca6ca85b30c942c2edc299801f31785b29a4
HERBIE LEWIS <HL298248@falmouth.ac.uk>
2024-04-29 16:43:15 +0100

    Updated health script and added hud test scene with simple graphic.

--- Assets/Scripts/Combat/CombatScript.cs
+++ Assets/Scripts/Combat/CombatScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CombatScript : MonoBehaviour
 {
     public int curHealth = 0;
-    public int MaxHealth = 100;
+    public int MaxHealth = 6;
     public GameObject entity;
     // Start is called before the first frame update
     void Start()
@@ -26,11 +26,47 @@ public class CombatScript : MonoBehaviour
     public void DamagePlayer(int damage)
     {
         curHealth -= damage;
+        healthBarChange();
 
 
     }
     public void TakeDamage(int damage)
     {
         curHealth -= damage;
+        healthBarChange();
+    }
+
+    public void healthBarChange()
+    {
+        switch(curHealth)
+        {
+            case 0:
+
+                // death screen
+                break;
+            case 1:
+                GameObject.Find("health_3").GetComponent<SpriteRenderer>().color = Color.yellow;
+                // one bar is left on orange
+                break;
+            case 2:
+                GameObject.Find("health_2").GetComponent<SpriteRenderer>().color = Color.red;
+                // one bar is white
+                break;
+            case 3:
+                GameObject.Find("health_2").GetComponent<SpriteRenderer>().color = Color.yellow;
+                // one bar is left white and the other turned orange
+                break;
+            case 4:
+                GameObject.Find("health_1").GetComponent<SpriteRenderer>().color = Color.red;
+                // two bars are white one is red
+                break;
+            case 5:
+                GameObject.Find("health_1").GetComponent<SpriteRenderer>().color = Color.yellow;
+                // two bars are white one is orange
+                break;
+            default: break;
+
+
+        }
     }
 }
