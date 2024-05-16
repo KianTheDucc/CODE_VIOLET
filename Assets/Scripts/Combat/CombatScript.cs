b253fba3a69e7041cdca493111c265e6ad7bd312
KB300310 <KB300310@falmouth.ac.uk>
2024-05-16 22:56:52 +0100

    Fixed Health system and UI.

--- Assets/Scripts/Combat/CombatScript.cs
+++ Assets/Scripts/Combat/CombatScript.cs
@@ -7,13 +7,24 @@ public class CombatScript : MonoBehaviour
     public int curHealth = 0;
     public int MaxHealth = 6;
     public GameObject entity;
+    public GameObject Health1;
+    public GameObject Health1H;
+    public GameObject Health2;
+    public GameObject Health2H;
+    public GameObject Health3;
+    public GameObject Health3H;
+
     // Start is called before the first frame update
     void Start()
     {
         curHealth = MaxHealth;
-        GameObject.Find("Health 1H").SetActive(false);
-        GameObject.Find("Health 2H").SetActive(false);
-        GameObject.Find("Health 3H").SetActive(false);
+        //GameObject.Find("Health 1H").SetActive(false);
+        //GameObject.Find("Health 2H").SetActive(false);
+        //GameObject.Find("Health 3H").SetActive(false);
+
+        Health1H.SetActive(false);
+        Health2H.SetActive(false);
+        Health3H.SetActive(false);
     }
 
     // Update is called once per frame
@@ -48,26 +59,34 @@ public class CombatScript : MonoBehaviour
                 // death screen
                 break;
             case 1:
-                GameObject.Find("Health 1H").SetActive(true);
-                GameObject.Find("Health 1").SetActive(false);
+                Health1H.SetActive(true);
+                Health1.SetActive(false);
+                //GameObject.Find("Health 1H").SetActive(true);
+                //GameObject.Find("Health 1").SetActive(false);
                 // one bar is left on orange
                 break;
             case 2:
-                GameObject.Find("Health 2H").SetActive(false);
+                Health2H.SetActive(false);
+                //GameObject.Find("Health 2H").SetActive(false);
                 // one bar is white
                 break;
             case 3:
-                GameObject.Find("Health 2").SetActive(false);
-                GameObject.Find("Health 2H").SetActive(true);
+                Health2.SetActive(false);
+                Health2H.SetActive(true);
+                //GameObject.Find("Health 2").SetActive(false);
+                //GameObject.Find("Health 2H").SetActive(true);
                 // one bar is left white and the other turned orange
                 break;
             case 4:
-                GameObject.Find("Health 3H").SetActive(false);
+                Health3H.SetActive(false);
+                //GameObject.Find("Health 3H").SetActive(false);
                 // two bars are white one is red
                 break;
             case 5:
-                GameObject.Find("Health 3").SetActive(false);
-                GameObject.Find("Health 3H").SetActive(true);
+                Health3.SetActive(false);
+                Health3H.SetActive(true);
+                //GameObject.Find("Health 3").SetActive(false);
+                //GameObject.Find("Health 3H").SetActive(true);
                 // two bars are white one is orange
                 break;
             default: break;
