e388c1a1d446f118915ff7d62bb8f5949da4b1f3
ES294335 <es294335@falmouth.ac.uk>
2024-05-22 01:30:47 +0100

    Changed GameFinishScript, added back purple filter

--- Assets/GameFinishScript.cs
+++ Assets/GameFinishScript.cs
@@ -4,21 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameFinishScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+{  
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
-        SceneManager.LoadSceneAsync("Main_Menu_Save_Load");
+        SceneManager.LoadSceneAsync("EndScene");
     }
 }
